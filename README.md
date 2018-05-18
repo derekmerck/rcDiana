3c7b63310d9aa28fbae9cc80e8f65f9f266bf285
derek <derek_merck@brown.edu>
2018-05-18 11:59:31 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## rcDiana 
+
+Merck, Spring 2017
+
+Use with `Resin.io` image
---
+++ base/conf_login.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Resetting root login password to $ROOT_PASSWORD"
+echo "root:$SSH_PASSWORD" | chpasswd
